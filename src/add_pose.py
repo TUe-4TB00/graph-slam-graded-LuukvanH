d95b0775c476e5f19e778f3e8b126ae0470dfb6e
20234243 <l.b.g.v.huisseling@student.tue.nl>
2026-05-27 20:27:22 +0200

    Implemented add_pose (3).

--- src/add_pose.py
+++ src/add_pose.py
@@ -9,15 +9,19 @@ ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # 
 MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (bearing, range)
 
 def add_pose(graph, initial_estimate):
+    
     # TODO: Add the odometry factor between X(4) and X(5) to the graph (BetweenFactorPose2)
-    odometry = gtsam.Pose2(math.sqrt(2), math.sqrt(2), math.pi / 2)
+    turn_1 = gtsam.Pose2(0.0, 0.0, math.radians(45))
+    move_forward = gtsam.Pose2(2.0, 0.0, 0.0)
+    turn_2 = gtsam.Pose2(0.0, 0.0, math.radians(45))
+    
+    odometry = turn_1.compose(move_forward).compose(turn_2)
     graph.add(gtsam.BetweenFactorPose2(X(3), X(4), odometry, ODOMETRY_NOISE))
 
     # TODO: Based on the odometry, find the initial estimate for the pose of X(5) and add it to the graph
-    exact_x = 4.0 + math.sqrt(2)
-    exact_y = math.sqrt(2)
-    exact_theta = math.pi / 2
+    pose_3 = initial_estimate.atPose2(X(3))
+    pose_4_guess = pose_3.compose(odometry)
     
-    initial_estimate.insert(X(4), gtsam.Pose2(exact_x, exact_y, exact_theta))
+    initial_estimate.insert(X(4), pose_4_guess)
 
     return graph, initial_estimate
